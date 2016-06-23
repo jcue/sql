ed124dc586070700b54973289ad684d338c1e159
Jose Cuellar <cuellar_jose@hotmail.com>
2016-06-22 22:43:54 -0700

    created new database

---
+++ sqla.py
@@ -0,0 +1,18 @@
+# Create a SQLite3 database and table
+
+# import the sqlite3 library
+import sqlite3
+
+# create a new database if the database doesn't already exist
+conn = sqlite3.connect("new.db")
+
+# get a cursor object used to execute SQL commands
+cursor = conn.cursor()
+
+# create table
+cursor.execute("""CREATE TABLE populateion
+				  (city TEXT, state Text, population INT)
+				  """)
+
+# close teh database connection
+conn.close()
